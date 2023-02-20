9a90d960a31fa51f60f4e19f84a8a48bb1da68e7
Bruno Alves da Silva <125697837+Lordkaly@users.noreply.github.com>
2023-02-20 18:37:31 -0300

    Update chatgptbypass.py

--- chatgptbypass.py
+++ chatgptbypass.py
@@ -3,39 +3,72 @@ import json
 import requests
 
 API_KEY_FILE = 'api_key.txt'
+API_URL = 'https://api.openai.com/v1/completions'
 
-if os.path.exists(API_KEY_FILE):
+# Verifica se o arquivo de chave de API existe e lê a chave
+try:
     with open(API_KEY_FILE) as f:
         api_key = f.read().strip()
-else:
-    api_key = input("API: ")
+except FileNotFoundError:
+    api_key = input("Please enter your OpenAI API key: ")
     with open(API_KEY_FILE, 'w') as f:
         f.write(api_key)
 
-while True:
-    write = input("Ask me anything: ")
+# Configuração padrão para solicitações à API
+model = 'text-davinci-003'
+max_tokens = 4000
+temperature = 1.0
 
+# Loop principal para permitir várias consultas
+while True:
+    # Solicita entrada do usuário
+    write = input("Ask me anything: ")
+    
+    # Verifica se a entrada não está vazia
+    if not write:
+        print('Please enter a prompt')
+        continue
+    
+    # Monta a solicitação à API
     headers = {
         'Content-Type': 'application/json',
         'Authorization': f'Bearer {api_key}'
     }
 
     data = {
-        'model': 'text-davinci-003',
+        'model': model,
         'prompt': write,
-        'max_tokens': 4000,
-        'temperature': 1.0
+        'max_tokens': max_tokens,
+        'temperature': temperature
     }
 
-    response = requests.post('https://api.openai.com/v1/completions', headers=headers, json=data)
+    # Envia a solicitação à API e verifica a resposta
+    response = requests.post(API_URL, headers=headers, json=data)
 
     if response.status_code == 200:
         response_data = json.loads(response.text)
         text = response_data['choices'][0]['text']
         print(text)
+
+        # Pergunta se o usuário deseja salvar a resposta em um arquivo
+        choice = input("Do you want to save the response to a file? [y/n]: ")
+        if choice.lower() == 'y':
+            # Solicita ao usuário um nome para o arquivo de saída
+            filename = input("Please enter a name for the output file (without extension): ")
+            filename += '.txt'
+
+            try:
+                # Grava a resposta no arquivo de saída
+                with open(filename, 'w') as f:
+                    f.write(text)
+                    print(f"Response saved to {filename}")
+            except Exception as e:
+                print(f"An error occurred while writing the file: {e}")
+
     else:
-        print('API error')
+        print('API error: ', response.text)
     
-    choice = input("Want to ask something else? [y/n]: ")
+    # Pergunta se o usuário deseja fazer outra consulta
+    choice = input("Do you want to ask something else? [y/n]: ")
     if choice.lower() != 'y':
         break
